a8f46643e0a398933d27a491559c87c1c66577ef
Vector-ops <sumith2827@gmail.com>
2024-07-29 20:29:32 +0530

    feat: implement tcp transport

---
+++ p2p/tcp_transport.go
@@ -0,0 +1,88 @@
+package p2p
+
+import (
+	"fmt"
+	"net"
+	"sync"
+)
+
+type TCPPeer struct {
+	conn net.Conn
+	// dial a connection -> outbound: true
+	// accept and retrieve a connection -> oubound: false
+	outbound bool
+}
+
+func NewTCPPeer(conn net.Conn, outbound bool) *TCPPeer {
+	return &TCPPeer{
+		conn:     conn,
+		outbound: outbound,
+	}
+}
+
+type TCPTransportOpts struct {
+	ListenAddr    string
+	HandshakeFunc HandshakeFunc
+	Decoder       Decoder
+}
+
+type TCPTransport struct {
+	TCPTransportOpts
+	listener net.Listener
+
+	mu    sync.RWMutex
+	peers map[net.Addr]Peer
+}
+
+func NewTCPTransport(opts TCPTransportOpts) *TCPTransport {
+	return &TCPTransport{
+		TCPTransportOpts: opts,
+	}
+}
+
+func (t *TCPTransport) ListenAndAccept() error {
+	var err error
+	t.listener, err = net.Listen("tcp", t.ListenAddr)
+	if err != nil {
+		return err
+	}
+
+	go t.acceptLoop()
+
+	return nil
+}
+
+func (t *TCPTransport) acceptLoop() {
+	for {
+		conn, err := t.listener.Accept()
+		if err != nil {
+			fmt.Printf("TCP accept loop error: %s\n", err)
+		}
+
+		fmt.Printf("New incoming connection: %+v\n", conn)
+		go t.handleConn(conn)
+	}
+}
+
+func (t *TCPTransport) handleConn(conn net.Conn) {
+	peer := NewTCPPeer(conn, true)
+
+	if err := t.HandshakeFunc(peer); err != nil {
+		conn.Close()
+		fmt.Printf("TCP handshake error: %s\n", err)
+
+		return
+	}
+
+	msg := &Message{}
+	for {
+		if err := t.Decoder.Decode(conn, msg); err != nil {
+			fmt.Printf("tcp error: %s\n", err)
+			continue
+		}
+
+		msg.From = conn.RemoteAddr()
+
+		fmt.Printf("message: %+v\n", msg)
+	}
+}
